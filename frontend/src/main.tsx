e60c1d5ed747ee8c17d8ddd5d3c6f73703400bed
murirop <panashemuriro2021@gmail.com>
2025-08-16 22:26:25 +0200

    remove unused modules

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
       <App />
-    </DndProvider>
   </React.StrictMode>,
 )
